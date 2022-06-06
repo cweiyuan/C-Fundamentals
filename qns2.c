60a5fcd4a7c84ed51b8d12ab2c35bd50287ebf13
cweiyuan <93870563+cweiyuan@users.noreply.github.com>
2022-06-06 18:47:45 +0800

    Question 2
    Write a program that computess the volume of a sphere with a 10-meter radius, using the formula v = 4/3πr³ . Write the fraction 4/3 as 4.0f/3.0f.

---
+++ qns2.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+   
+    float volume = (4/(3 * M_PI * pow(10, 3)));
+  
+    printf("The volume of the sphere is:%f", volume);
+    
+    return 0; 
+}
+
